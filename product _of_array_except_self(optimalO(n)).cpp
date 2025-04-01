3ddf9f215673c1ff8cffa8be776f48158e567fcc
AakashNamdeo1 <166501506+AakashNamdeo1@users.noreply.github.com>
2025-04-01 16:58:03 +0530

    Update product _of_array_except_self(optimalO(n)).cpp

--- product _of_array_except_self(optimalO(n)).cpp
+++ product _of_array_except_self(optimalO(n)).cpp
@@ -20,7 +20,7 @@ class solution
         //suffix
         for(int i=n-2;i>=0;i--)
         {
-           suffix[i]=suffix[i-1]*nums[i+1];
+           suffix[i]=suffix[i+1]*nums[i+1];
         }
         for(int i=0;i<n;i++)
         {
